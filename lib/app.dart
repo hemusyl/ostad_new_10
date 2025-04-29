4ca664e60201f529e928d24dc4063931a8dec4d5
hemusyl <humayun1278@gmail.com>
2025-04-29 15:21:39 -0400

    Ostad one

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'os10_button.dart';
+import 'form_login.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'batch 10 flutter',
+      home: LoginPage(),
+    );
+  }
+}
